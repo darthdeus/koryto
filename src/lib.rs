de4e5898353e1cada0859cf3f30b1f89e832bb87
Jakub Arnold <darthdeus@gmail.com>
2023-07-04 17:21:54 +0200

    Working RawWaker inspired by macroquad

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::new_without_default)]
 
 use core::future::Future;
-use std::pin::Pin;
+use std::{pin::Pin, task::{RawWaker, RawWakerVTable, Waker}};
 
 pub struct Coroutine {
     pub future: Pin<Box<dyn Future<Output = ()> + 'static>>
@@ -9,6 +9,19 @@ pub struct Coroutine {
 
 pub struct Koryto {
     pub coroutines: Vec<Coroutine>,
+}
+
+fn make_waker_vtable() -> RawWaker {
+    unsafe fn clone(data: *const ()) -> RawWaker {
+        RawWaker::new(data, &VTABLE)
+    }
+    unsafe fn wake(_data: *const ()) {}
+    unsafe fn wake_by_ref(_data: *const ()) {}
+    unsafe fn drop(_data: *const ()) {}
+
+    static VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
+
+     RawWaker::new(std::ptr::null(), &VTABLE)
 }
 
 impl Koryto {
@@ -20,13 +33,12 @@ impl Koryto {
         self.coroutines.push(Coroutine { future: Box::pin(co) });
     }
 
-    pub fn poll_coroutines(&mut self, delta: f32) {
-        self.coroutines.retain_mut(|co| {
-            // :derppat:
-            let f = co.future.as_mut();
-            let waker = waker_ref(co.future);
-            let context = std::task::Context::from_waker(&waker);
+    pub fn poll_coroutines(&mut self, _delta: f32) {
+        let raw_waker = make_waker_vtable();
+        let waker = unsafe { Waker::from_raw(raw_waker) };
+        let mut context = std::task::Context::from_waker(&waker);
 
+        self.coroutines.retain_mut(|co| {
             !co.future.as_mut().poll(&mut context).is_ready()
         });
     }
